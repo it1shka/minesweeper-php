905e046fdee673462942fa3112f0e976feda488e
it1shka <2tishbel@gmail.com>
2023-08-12 00:21:29 +0200

    Changed from iterators to arrays

--- includes/board.php
+++ includes/board.php
@@ -7,6 +7,6 @@ require_once "utils.php";
 
 #[Pure] function create_empty_real_board(int $cell_count): string {
     $range = arrange(0, $cell_count);
-    $cell_strings = map($range, fn($i) => create_cell($i, CELL_UNREVEALED));
-    return reduce($cell_strings, "", fn($a, $b) => $a . $b);
+    $cells = array_map(fn($i) => create_cell($i, CELL_UNREVEALED), $range);
+    return implode("", $cells);
 }
--- includes/utils.php
+++ includes/utils.php
@@ -2,23 +2,13 @@
 
 use JetBrains\PhpStorm\Pure;
 
-#[Pure] function arrange(int $start, int $end): iterable {
-    for ($i = $start; $i < $end; $i++) {
-        yield $i;
-    }
-}
-
-#[Pure] function map(iterable $stream, callable $func): iterable {
-    foreach ($stream as $key => $value) {
-        $result = $func($value, $key);
-        yield $result;
-    }
-}
-
-#[Pure] function reduce(iterable $stream, mixed $start_value, callable $reducer): mixed {
-    $output = $start_value;
-    foreach ($stream as $key => $value) {
-        $output = $reducer($output, $value, $key);
-    }
-    return $output;
+#[Pure] function arrange(int $start, int $end): array {
+    return match (true) {
+        $start >= $end => [],
+        default => (function() use ($start, $end) {
+            $head = $start;
+            $tail = arrange($start + 1, $end);
+            return [$head, ...$tail];
+        })()
+    };
 }
