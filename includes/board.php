f2d19bcf14526cd5509fc621503a0d1c35b36a34
it1shka <2tishbel@gmail.com>
2023-08-12 03:18:11 +0200

    continue developing board

--- includes/board.php
+++ includes/board.php
@@ -7,6 +7,14 @@ require_once "utils.php";
 
 #[Pure] function create_empty_real_board(int $cell_count): string {
     $range = arrange(0, $cell_count);
-    $cells = array_map(fn($i) => create_cell($i, CELL_UNREVEALED), $range);
+    $cells = array_map(function($number) {
+        return create_cell($number, CELL_UNREVEALED);
+    }, $range);
     return implode("", $cells);
+}
+
+#[Pure] function generate_bombs(int $board_size, int $bombs_amount): array {
+    $size_range = arrange(0, $board_size);
+    $positions = cross_product($size_range, $size_range);
+    return sample($positions, $bombs_amount);
 }
--- includes/utils.php
+++ includes/utils.php
@@ -5,9 +5,38 @@ use JetBrains\PhpStorm\Pure;
 #[Pure] function arrange(int $start, int $end): array {
     return match (true) {
         $start >= $end => [],
-        default => (function() use ($start, $end) {
+        default => (function () use ($start, $end) {
             $head = $start;
             $tail = arrange($start + 1, $end);
+            return [$head, ...$tail];
+        })()
+    };
+}
+
+#[Pure] function flatten(array $original): array {
+    return array_reduce($original, function ($acc, $elem) {
+        return match(gettype($elem)) {
+            "array" => [...$acc, ...flatten($elem)],
+            default => [...$acc, $elem]
+        };
+    }, []);
+}
+
+#[Pure] function cross_product(array $first, array $second): array {
+    $output = array_map(function($x) use (&$second) {
+        return array_map(fn($y) => [$x, $y], $second);
+    }, $first);
+    return flatten($output);
+}
+
+#[Pure] function sample(array $original, int $sample_size): array {
+    return match($sample_size) {
+        0 => [],
+        default => (function () use (&$original, $sample_size) {
+            $index = rand(0, count($original) - 1);
+            $head = $original[$index];
+            $next_array = array_filter($original, fn($_, $i) => $i !== $index);
+            $tail = sample($next_array, $sample_size - 1);
             return [$head, ...$tail];
         })()
     };
